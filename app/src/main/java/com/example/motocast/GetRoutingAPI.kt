7dd3ff0728bf58c64555abfa194c1058cba58dcb
gormrs <gormrs@uio.no>
2023-03-21 14:05:10 +0100

    key

--- app/src/main/java/com/example/motocast/GetRoutingAPI.kt
+++ app/src/main/java/com/example/motocast/GetRoutingAPI.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class GetRoutingAPI {
     private val BASE_URL = "https://api.openrouteservice.org/v2/"
-    private val apiKey = "5b3ce3597851110001cf624844b3031074b6423697be6ece55076f23" // Empty to avoid scraping
+    private val apiKey = "" // Empty to avoid scraping
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
         .baseUrl(BASE_URL).build()
