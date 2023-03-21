be036d8f13f8b3a23c450a5866823f7ce5c2326b
gormrs <gormrs@uio.no>
2023-03-21 13:06:35 +0100

    fixed gsonconverterfactory

--- app/src/main/java/com/example/motocast/GetRoutingAPI.kt
+++ app/src/main/java/com/example/motocast/GetRoutingAPI.kt
@@ -1,17 +1,20 @@
 package com.example.motocast
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class GetRoutingAPI {
-    private val BASEURL = "https://api.openrouteservice.org/v2/"
-    private val apiKey = "" // Empty to avoid scraping
-    private val retrofit = Retrofit.Builder().baseUrl(BASEURL).build()
+    private val BASE_URL = "https://api.openrouteservice.org/v2/"
+    private val apiKey = "5b3ce3597851110001cf624844b3031074b6423697be6ece55076f23" // Empty to avoid scraping
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL).build()
 
 
 
     fun getRoutingAPI() {
     }
-    // TODO: Implement the ViewModel
+
 
     }
