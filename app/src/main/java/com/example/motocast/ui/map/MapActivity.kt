48e22ab2212d6828325137fe476d2a547bf68d80
Gorm <gormrs@uio.no>
2023-03-31 11:55:12 +0200

    La til location Puck med mapViwe.location, og cameraOptions hardcode til OJD

--- app/src/main/java/com/example/motocast/ui/map/MapActivity.kt
+++ app/src/main/java/com/example/motocast/ui/map/MapActivity.kt
@@ -13,9 +13,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.motocast.R
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.ResourceOptionsManager
 import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.locationcomponent.LocationProvider
+import com.mapbox.maps.plugin.locationcomponent.location
+
+import com.mapbox.maps.plugin.locationcomponent.location2
 
 
 @Composable
@@ -63,14 +69,22 @@ fun rememberMapViewWithLifecycle(): MapView {
     val mapView = remember {
         MapView(context).apply {
             val mapView = this
-            mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS){
-                val mapboxMap = this
-
+            mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS) { style ->
+                val mapboxMap = mapView.getMapboxMap()
+                mapView.location.apply {
+                    enabled = true
+                    pulsingEnabled = true
+                }
+                val cameraOptions = CameraOptions.Builder()
+                    .center(Point.fromLngLat(10.717173333333333, 59.942881666666665))
+                    .zoom(15.0)
+                    .build()
+                mapboxMap.setCamera(cameraOptions)
             }
         }
     }
-
     return mapView
 }
+
 
 
