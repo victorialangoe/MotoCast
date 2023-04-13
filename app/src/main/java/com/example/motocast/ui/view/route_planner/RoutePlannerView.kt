44421e0a4eeeaa80e2b2ba6118fab6666a288141
Espen Noreng <espennoreng@gmail.com>
2023-04-13 13:13:15 +0200

    Bug fix: Made routePlannerViewScrollable

--- app/src/main/java/com/example/motocast/ui/view/route_planner/RoutePlannerView.kt
+++ app/src/main/java/com/example/motocast/ui/view/route_planner/RoutePlannerView.kt
@@ -4,26 +4,18 @@ import android.content.Context
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import com.example.motocast.ui.view.route_planner.buttons.AddFieldButton
 import com.example.motocast.ui.view.route_planner.buttons.ClearAllButton
 import com.example.motocast.ui.view.route_planner.buttons.CreateRouteButton
 import com.example.motocast.ui.view.route_planner.buttons.DestinationButton
-import com.example.motocast.ui.view.route_planner.date_and_time.DatePicker
 import com.example.motocast.ui.view.route_planner.date_and_time.DateTimePicker
-import com.example.motocast.ui.view.route_planner.date_and_time.TimePicker
 import com.example.motocast.ui.viewmodel.route_planner.DatePickerUiState
 import com.example.motocast.ui.viewmodel.route_planner.Destination
-import com.example.motocast.ui.viewmodel.route_planner.RoutePlannerViewModel
 import com.example.motocast.ui.viewmodel.route_planner.TimePickerUiState
 
 @Composable
@@ -54,45 +46,48 @@ fun RoutePlannerView(
     ) {
 
         RouteHeader(onClick = { navigateTo("home_screen") })
+        // This makes the content scrollable
+        LazyColumn(content = {
+            item {
+                Spacer(modifier = Modifier.height(20.dp))
 
-        Spacer(modifier = Modifier.height(20.dp))
+                // This is where all the added destinations will be shown (if any)
+                destinations.forEachIndexed { destinationIndex, _ ->
+                        Spacer(modifier = Modifier.height(16.dp)) // Add a gap between the items
+                        DestinationButton(
+                            destinationIndex = destinationIndex,
+                            destinations = destinations,
+                            editDestination = { editDestination(destinationIndex) },
+                            removeDestination = { removeDestination(destinationIndex) }
+                        )
+                    }
 
-        // This is where all the added destinations will be shown (if any)
-        LazyColumn {
-            items(destinations.size) { destinationIndex ->
-                Spacer(modifier = Modifier.height(16.dp)) // Add a gap between the items
-                DestinationButton(
-                    destinationIndex = destinationIndex,
-                    destinations = destinations,
-                    editDestination = { editDestination(destinationIndex) },
-                    removeDestination = { removeDestination(destinationIndex) }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                AddFieldButton { addDestination() }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                DateTimePicker(
+                    context = context,
+                    updateDateUiState = { updateDateUiState(it) },
+                    updateTimeUiState = { updateTimeUiState(it) },
+                    year = year,
+                    month = month,
+                    day = day,
+                    hour = hour,
+                    minute = minute,
                 )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                CreateRouteButton { startRoute() }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                ClearAllButton(clearAll = { clearAll() })
             }
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        AddFieldButton { addDestination() }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        DateTimePicker(
-            context = context,
-            updateDateUiState = { updateDateUiState(it) },
-            updateTimeUiState = { updateTimeUiState(it) },
-            year = year,
-            month = month,
-            day = day,
-            hour = hour,
-            minute = minute,
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        CreateRouteButton { startRoute() }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        ClearAllButton(clearAll = { clearAll() })
+        })
     }
 }
